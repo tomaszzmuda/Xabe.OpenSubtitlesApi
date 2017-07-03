b02eb63753212a870d35b36671f57051815e3558
tzmuda <tomasz.a.zmuda@gmail.com>
2017-07-03 19:19:44 +0200

    Initial

---
+++ Xabe.OpenSubtitlesApi/Controllers/OpenSubtitles.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using OSDBnet;
+using OSDBnet.Backend;
+using RestSharp;
+
+namespace Xabe.OpenSubtitlesApi.Controllers
+{
+    public class OpenSubtitlesController: ApiController
+    {
+        public async Task<IHttpActionResult> GetByHash(string hash)
+        {
+            var client = Osdb.Login("OSTestUserAgentTemp");
+
+            var request = new SearchSubtitlesRequest
+            {
+                imdbid = string.Empty,
+                query = string.Empty,
+                moviehash = hash,
+                sublanguageid = "pol"
+            };
+
+            MethodInfo dynMethod = client.GetType()
+                                         .GetMethod("SearchSubtitlesInternal",
+                                             BindingFlags.NonPublic | BindingFlags.Instance);
+            var subtitlesList = (List<Subtitle>) dynMethod.Invoke(client, new object[] {request});
+            var subtitles = subtitlesList.OrderByDescending(x => x.Rating)
+                                         .FirstOrDefault();
+            var subtitlesPath = client.DownloadSubtitleToPath(Path.GetTempPath(), subtitles);
+
+
+            string srt = ConvertToSrt(subtitlesPath);
+
+            return Ok(Encoding.UTF8.GetBytes(srt));
+        }
+
+        private string ConvertToSrt(string path)
+        {
+            var client = new RestClient("https://opensubtitles-subtitle-tools.p.mashape.com/convert");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("X-Mashape-Key", Environment.GetEnvironmentVariable("MASHAP_API_KEY"));
+            request.AddFileBytes("file", File.ReadAllBytes(path), "file.txt");
+            request.AddParameter("convert_to", "srt");
+            request.AddParameter("output_charset_encoding", "UTF-8");
+            IRestResponse<Model.Root> response = client.Execute<Model.Root>(request);
+            return response.Data.converted;
+        }
+    }
+}
---
+++ Xabe.OpenSubtitlesApi/Models/Model.cs
@@ -0,0 +1,63 @@
+﻿namespace Xabe.OpenSubtitlesApi
+{
+    public class Model
+    {
+        public class Root
+        {
+            public string sent { get; set; }
+            public Infos infos { get; set; }
+            public string converted { get; set; }
+            public Messages messages { get; set; }
+            public float process_time { get; set; }
+        }
+
+        public class Infos
+        {
+            public string language { get; set; }
+            public string language_iso { get; set; }
+            public Language_Reliable language_reliable { get; set; }
+            public Input input { get; set; }
+            public Output output { get; set; }
+        }
+
+        public class Language_Reliable
+        {
+            public string name { get; set; }
+            public string code { get; set; }
+            public bool reliable { get; set; }
+        }
+
+        public class Input
+        {
+            public float fps { get; set; }
+            public string charset_encoding { get; set; }
+            public string data_compression { get; set; }
+            public string sub_format { get; set; }
+            public int words { get; set; }
+            public int chars { get; set; }
+            public string first_timestamp { get; set; }
+            public string last_timestamps { get; set; }
+        }
+
+        public class Output
+        {
+            public string fps { get; set; }
+            public string charset_encoding { get; set; }
+            public string sub_format { get; set; }
+            public int words { get; set; }
+            public int chars { get; set; }
+            public string first_timestamp { get; set; }
+            public string last_timestamps { get; set; }
+            public string data_encoding { get; set; }
+            public string stripped_html { get; set; }
+        }
+
+        public class Messages
+        {
+            public string timeshift { get; set; }
+            public string framerate { get; set; }
+            public string convert_to { get; set; }
+            public string output_data_encoding { get; set; }
+        }
+    }
+}
